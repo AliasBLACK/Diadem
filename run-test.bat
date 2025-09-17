4af51d8a9298fb4f752c88b00f4195546934af2d
Xuanming Zhou <xm@alias.black>
2025-09-17 10:33:35 +0800

    Removed test bat file

--- run-test.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "d:\Documents\GitHub\Diadem"
-java -cp "target\classes;%USERPROFILE%\.m2\repository\org\graalvm\polyglot\polyglot\24.2.2\polyglot-24.2.2.jar;%USERPROFILE%\.m2\repository\org\graalvm\polyglot\js-community\24.2.2\js-community-24.2.2.jar;%USERPROFILE%\.m2\repository\org\lwjgl\lwjgl\3.3.3\lwjgl-3.3.3.jar;%USERPROFILE%\.m2\repository\org\lwjgl\lwjgl-glfw\3.3.3\lwjgl-glfw-3.3.3.jar;%USERPROFILE%\.m2\repository\org\lwjgl\lwjgl-opengl\3.3.3\lwjgl-opengl-3.3.3.jar;%USERPROFILE%\.m2\repository\org\lwjgl\lwjgl\3.3.3\lwjgl-3.3.3-natives-windows.jar;%USERPROFILE%\.m2\repository\org\lwjgl\lwjgl-glfw\3.3.3\lwjgl-glfw-3.3.3-natives-windows.jar;%USERPROFILE%\.m2\repository\org\lwjgl\lwjgl-opengl\3.3.3\lwjgl-opengl-3.3.3-natives-windows.jar" black.alias.diadem.ThreeJSTest
-pause
